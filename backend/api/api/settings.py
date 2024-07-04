985ea8eee4057828b949e69ce5f6ca1cffa4c491
Josh Reese <droptablestar@gmail.com>
2024-07-04 14:12:03 -0400

    drf snippets

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -9,24 +9,24 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(pfz8+-m76+q-g1)0z3=*hpakf!uu_!-8g5o=kvtlxju752&*2'
+SECRET_KEY = 'django-insecure-(%8l6yoex$vtf4xfc8y!^s$upnv7%i&k6c!t67z55%6xb$)70^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-SITE_ID = 1
 
 # Application definition
 
@@ -37,14 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django.contrib.sites",
-    # 'rest_framework',
-    # 'rest_framework.authtoken',
-    # "allauth",
-    # "allauth.account",
-    # "allauth.socialaccount",  # social authentication
-    # "dj_rest_auth",
-    # "dj_rest_auth.registration",
+    'rest_framework',
+    'snippets'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -84,12 +77,8 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER_NAME"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -112,11 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
-    ]
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -15,17 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-
-from api.main import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include(router.urls)),
+    path('', include('snippets.urls')),
+    path('api-auth/', include('rest_framework.urls'))
+    # path('admin/', admin.site.urls),
 ]
